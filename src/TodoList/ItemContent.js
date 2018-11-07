6fb808f64a98943a20ad5c250cf46cc9b880e694
Farid <farid.hammouten@gmail.com>
2018-11-08 01:47:47 +0900

    doneTodo is working, deleteTodo not yet and every ListGroupItem do not received Item from TodoList nor value

---
+++ src/TodoList/ItemContent.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+import { 
+    ListGroupItem,
+
+} from 'react-bootstrap';
+import FontAwesomeIcon from 'react-fontawesome';
+import TodoList from './TodoList';
+
+
+class ItemContent extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            completed: false
+        }
+    }
+
+    deleteTodo(event){
+       let ts = event;
+       console.log(ts)
+       let parent = event.currentTarget.parentNode;
+       console.log(parent);
+    }
+
+
+
+    doneTodo(){
+        // console.log(this.state.completed)
+        this.setState({
+            completed: !this.state.completed
+        });
+    }
+
+    render(){
+        
+            return (
+                <ListGroupItem as='li' className={`text-left ${this.state.completed ? 'list-group-item-success': null}`}>
+                    <FontAwesomeIcon 
+                        name={`fas fa-window-close`}
+                        className={`close`}
+                        onClick={this.deleteTodo.bind(this)}
+                    />
+                    <FontAwesomeIcon 
+                        name={`fas fa-check-square`} 
+                        className={`close`}
+                        onClick={this.doneTodo.bind(this)}
+                    />
+                </ListGroupItem>
+            )
+          
+    }
+}
+
+export default ItemContent
